d74e9d83efddd466848381fcbe4369a09d04219b
yaritxa <yaritxa.duarte@estudiantesunibague.edu.co>
2026-04-20 21:58:49 -0500

    toda la parte de las rutinas con el Css y el HTML

--- rutinas/urls.py
+++ rutinas/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mis_rutinas, name='mis_rutinas'),
+    path('', views.lista_rutinas, name='lista_rutinas'),
     path('crear/', views.crear_rutina, name='crear_rutina'),
+    path('<int:pk>/editar/', views.editar_rutina, name='editar_rutina'),
+    path('<int:pk>/eliminar/', views.eliminar_rutina, name='eliminar_rutina'),
+    path('<int:pk>/', views.detalle_rutina, name='detalle_rutina'),
 ]
